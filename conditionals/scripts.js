5fad3c3be02698a89de9025657cfc9241cbaa760
Moses Gang <docgang11@gmail.com>
2023-05-24 12:34:31 +0300

    learned about else if statement

--- conditionals/scripts.js
+++ conditionals/scripts.js
@@ -1,9 +1,20 @@
-let workDone = false;
-let allowance;
+// IF STATEMENT
+// let workDone = false;
+// let allowance;
 
-if (workDone == true) {
-  allowance = "$50";
-} else {
-  allowance = "$10";
-}
-alert(allowance);
+// if (workDone == true) {
+//   allowance = "$50";
+// } else {
+//   allowance = "$10";
+// }
+// alert(allowance);
+
+// ELSE IF STATEMENT
+// let cheese = "cheddar";
+// if (cheese == "ched") {
+//   alert("you are going to have cheese on toast today for breakfast");
+// } else if (cheese) {
+//   alert("you aint having it today bro");
+// } else {
+//   alert("mmmhhhh!");
+// }
